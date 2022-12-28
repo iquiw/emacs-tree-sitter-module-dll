2c2f97cd28032d65ebcc62bc72c8aa9454d8f2b4
Iku Iwasa <iku.iwasa@gmail.com>
2022-12-29 08:46:21 +0900

    Add typescript/tsx languages
    * Changed build.sh to build 2 languages in a repository.

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
-#! /bin/sh
+#! /bin/bash
 
 set -euo pipefail
+
+CC=cc
+
+build() {
+        local dir=$1
+        local lang=$2
+        local dest=$3
+
+        cp "$dir/grammar.js" "$dir/src"
+        pushd "$dir/src" >/dev/null
+
+        $CC -fPIC -c -I. parser.c
+
+        if [ -f scanner.c ]; then
+                $CC -fPIC -c -I. scanner.c
+        elif [ -f scanner.cc ]; then
+                CC=c++
+                $CC -fPIC -I. -c scanner.cc
+        fi
+
+        $CC -fPIC -shared ./*.o -o "$dest/libtree-sitter-${lang}.dll"
+        rm -f grammar.js ./*.o
+        popd >/dev/null
+}
 
 dir=$1
 if [ "$dir" = . ]; then
@@ -12,23 +36,19 @@ fi
 
 echo -n "Building $lang... "
 
-cp "$dir/grammar.js" "$dir/src"
-cd "$dir/src"
-
-CC=cc
-
-$CC -fPIC -c -I. parser.c
-
-if [ -f scanner.c ]; then
-	$CC -fPIC -c -I. scanner.c
-elif [ -f scanner.cc ]; then
-	CC=c++
-	$CC -fPIC -I. -c scanner.cc
+if [ "$lang" = typescript ]; then
+        dest=../../../dist
+        build "$dir/typescript" typescript "$dest"
+        build "$dir/tsx" tsx "$dest"
+else
+        dest=../../dist
+        build "$dir" "$lang" "$dest"
 fi
 
-$CC -fPIC -shared *.o -o "../../dist/libtree-sitter-${lang}.dll"
-rm -f grammar.js *.o
-
-cp ../LICENSE "../../dist/licenses/LICENSE-$lang"
+if [ "$dir" = . ]; then
+        cp "$dir/LICENSE" "../dist/licenses/LICENSE-$lang"
+else
+        cp "$dir/LICENSE" "dist/licenses/LICENSE-$lang"
+fi
 
 echo "done."
