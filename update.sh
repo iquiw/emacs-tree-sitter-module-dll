4e31eb6e4a6bbb100f185be67b2fb50bc05882dd
Iku Iwasa <iku.iwasa@gmail.com>
2023-01-08 17:20:44 +0900

    Setup GitHub Action to update language submodules

---
+++ update.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -eo pipefail
+
+check_changed() {
+        mods=$(git status --porcelain | awk '/^ M/ { if (mods == "") { mods=$2 } else { mods=mods "," $2 } } END { print mods }')
+        if [ -n "$mods" ]; then
+                echo modules="$mods"
+                return 0
+        else
+                return 1
+        fi
+}
+
+git submodule foreach sh -c 'git fetch && git switch -d origin/HEAD'
+
+if [ -n "$GITHUB_OUTPUT" ]; then
+        if check_changed >> "$GITHUB_OUTPUT"; then
+                date +%Y%m%d > tag.txt
+                git submodule status > language-manifest.txt
+        fi
+else
+        check_changed
+fi
+
